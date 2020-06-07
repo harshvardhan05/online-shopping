36364c8432094f39180fc38e4e471761500e3302
Harshvardhan <hv050694@gmail.com>
2020-06-08 03:20:37 +0530

    My Project Setup Completed Successfully

---
+++ shoppingbackend/src/main/java/net/hv/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.hv.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
